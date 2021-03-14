ac67c28e0f8615dc1815147fa34a073a9940d7f0
ianes1978 <roberto.ianes.78@gmail.com>
2021-03-14 18:56:40 +0100

    .

--- src/index.ts
+++ src/index.ts
@@ -6,18 +6,6 @@ const app = express();
 
 bootServices(app)
 
-
-
-
-const pippo:string = 'Funziona';
-
-function prova (a:string){
-    console.log(a)
-
-}
-
-prova(pippo);
-
 app.listen(PORT, () => {
     console.log('server started at http://localhost:' + PORT);
-  });
+});
--- src/services/index.bs.ts
+++ src/services/index.bs.ts
@@ -5,7 +5,7 @@ import { Express } from 'express';
 codegen`
     var glob = require("glob")
         const list=  glob.sync("**/*.route.ts",{cwd:__dirname})
-        const {paths,filenames,imports,objNames,appUseRoute} = list.reduce((acc,pathRoute)=>{
+        const {paths,filenames,imports,objNames,appUseRoute,APIs} = list.reduce((acc,pathRoute)=>{
            const filename = pathRoute.split('/').slice(-1);
            const path = pathRoute.split('/').slice(0,-1);
            acc.paths.push('/'+path.join('/')+'/');
@@ -13,11 +13,11 @@ codegen`
            const objName = filename[0].split('.').slice(0,1)[0];
            acc.objNames.push(objName);
            acc.imports.push('import '+objName+' from "./'+pathRoute+'";'); 
+           acc.APIs.push("/"+path.join('/')+"/"+objName)
            acc.appUseRoute.push("app.use('/"+path.join('/')+"/"+objName+"', "+objName+");"); 
-       
            return acc
-        },{paths:[],filenames:[],imports:[],objNames:[],appUseRoute:[]})
-        console.log(appUseRoute)
+        },{paths:[],filenames:[],imports:[],objNames:[],appUseRoute:[],APIs:[]})
+        console.log('API:',APIs)
        
     module.exports = [
         imports.join(''),
